dc9a0ed4b59bdc760fa79093bd162ab926eb0b42
BobbyDigitalDev <bobbyl76@gmail.com>
2015-08-06 18:15:02 -0400

    home stretch

--- config.rb
+++ config.rb
@@ -2,5 +2,5 @@ require "susy"
 css_dir = '_/css'
 sass_dir = '_/components/sass'
 javascripts_dir = '_/js'
-output_style = :nested
+output_style = :expanded
 relative_assets = true; #this tells the configuration file to allow relative asset linking so we can link to our hero image using a relative path
